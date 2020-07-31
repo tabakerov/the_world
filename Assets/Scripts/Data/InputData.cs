8f7eedea408dba23f08d2dfd8534fe0b0e77d0ed
Dmitry Tabakerov <tabakerov@outlook.com>
2020-08-01 00:51:12 +0300

    something's working

---
+++ Assets/Scripts/Data/InputData.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public struct InputData : IComponentData
+{
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+}
---
+++ Assets/Scripts/Systems/PlayerMovementSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class PlayerMovementSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+        Entities.ForEach(
+            (ref Translation translation, in MoveData moveData) 
+            => 
+            {
+                float3 normalizeDir = math.normalizesafe(moveData.direction);
+                translation.Value += normalizeDir * moveData.speed * deltaTime;
+            })
+        .Run();
+    }
+}
