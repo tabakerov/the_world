9ec57934abc30d207c265988fb70ae75749ad719
Dmitry Tabakerov <tabakerov@outlook.com>
2020-08-01 23:51:05 +0300

    start implementing hunting/runaway behavior

---
+++ Assets/Scripts/Systems/HuntingSystem.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class HuntingSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+       // float deltaTime = Time.DeltaTime;
+        Entities.WithAll<PredatorTag>().ForEach(
+            (ref Translation translation, in MoveData moveData)
+            =>
+            {
+                translation.Value.x += moveData.speed * 0.1f;
+            }
+            ).Schedule();
+    }
+}
---
+++ Assets/Scripts/Systems/RunawaySystem.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class RunawaySystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+       // float deltaTime = Time.DeltaTime;
+        Entities.WithAll<HerbivoreTag>().ForEach(
+            (ref Translation translation, in MoveData moveData)
+            =>
+            {
+                translation.Value.x -= moveData.speed * 0.1f;
+            }
+            ).Schedule();
+    }
+}
