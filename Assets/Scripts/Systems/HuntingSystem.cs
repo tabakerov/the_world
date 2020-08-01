d42ce67105022d7352525b893eb2ccd8b8986932
Dmitry Tabakerov <tabakerov@outlook.com>
2020-08-02 00:34:22 +0300

    they're running somewhere

--- Assets/Scripts/Systems/HuntingSystem.cs
+++ Assets/Scripts/Systems/HuntingSystem.cs
@@ -1,18 +1,33 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using Unity.Collections;
 
 public class HuntingSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-       // float deltaTime = Time.DeltaTime;
+        // float deltaTime = Time.DeltaTime;
+        EntityQuery entityQuery = GetEntityQuery(typeof(HerbivoreTag), ComponentType.ReadOnly<Translation>());
+        NativeArray<Translation> preys = entityQuery.ToComponentDataArray<Translation>(Unity.Collections.Allocator.TempJob);
+
         Entities.WithAll<PredatorTag>().ForEach(
-            (ref Translation translation, in MoveData moveData)
+            (ref Translation translation, ref MoveData moveData)
             =>
             {
-                translation.Value.x += moveData.speed * 0.1f;
+                // get closest prey
+                float3 nearestPreyPosition = new float3(float.MaxValue, float.MaxValue, float.MaxValue);
+                for (int i = 0; i < preys.Length; i++)
+                {
+                    if (math.distancesq(translation.Value, preys[i].Value) < math.distancesq(nearestPreyPosition, preys[i].Value))
+                    {
+                        nearestPreyPosition = preys[i].Value;
+                    }
+                }
+
+                // set MoveData to direction away from neares predator
+                moveData.direction = math.normalizesafe(nearestPreyPosition - translation.Value);
             }
-            ).Schedule();
+            ).ScheduleParallel();
     }
 }
--- Assets/Scripts/Systems/RunawaySystem.cs
+++ Assets/Scripts/Systems/RunawaySystem.cs
@@ -1,18 +1,35 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
+using Unity.Collections;
 
 public class RunawaySystem : SystemBase
 {
     protected override void OnUpdate()
     {
-       // float deltaTime = Time.DeltaTime;
+        // float deltaTime = Time.DeltaTime;
+        EntityQuery entityQuery = GetEntityQuery(typeof(PredatorTag), ComponentType.ReadOnly<Translation>());
+        NativeArray<Translation> predators = entityQuery.ToComponentDataArray<Translation>(Unity.Collections.Allocator.TempJob);
+
         Entities.WithAll<HerbivoreTag>().ForEach(
-            (ref Translation translation, in MoveData moveData)
+            (ref Translation translation, ref MoveData moveData)
             =>
             {
-                translation.Value.x -= moveData.speed * 0.1f;
+                // get closest predator
+                float3 nearestPredatorPosition = new float3(float.MaxValue, float.MaxValue, float.MaxValue);
+                for (int i = 0; i < predators.Length; i++)
+                {
+                    if (math.distancesq(translation.Value, predators[i].Value) < math.distancesq(nearestPredatorPosition, predators[i].Value))
+                    {
+                        nearestPredatorPosition = predators[i].Value;
+                    }
+                    Debug.Log(nearestPredatorPosition);
+                }
+
+                // set MoveData to direction away from neares predator
+                moveData.direction = math.normalizesafe(translation.Value - nearestPredatorPosition);
             }
-            ).Schedule();
+            ).ScheduleParallel();
     }
 }
